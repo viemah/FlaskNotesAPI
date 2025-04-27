6362e63bd1d5ca4d1c0aa7f3572972728822d0dd
Vie Mahatharadol <158812634+viemah@users.noreply.github.com>
2025-04-28 02:01:01 +0800

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    notes = db.relationship("Note", backref="owner", lazy="dynamic")
+
+    def set_password(self, pwd):
+        self.password_hash = generate_password_hash(pwd)
+
+    def check_password(self, pwd):
+        return check_password_hash(self.password_hash, pwd)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
